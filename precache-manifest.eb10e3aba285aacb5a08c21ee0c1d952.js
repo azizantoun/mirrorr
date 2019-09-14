50648dc909e983a39fd9f66b5a649c66899e59e3
Aziz Antoun <azizantoun@gmail.com>
2019-09-14 15:24:05 +0300

    Updates

---
+++ precache-manifest.eb10e3aba285aacb5a08c21ee0c1d952.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12ad7a0360ff7fa50c0d668710f40508",
+    "url": "/azizantoun/mirrorr/index.html"
+  },
+  {
+    "revision": "144fc08f40d10add00ce",
+    "url": "/azizantoun/mirrorr/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d99bc364a6097fbaed1e",
+    "url": "/azizantoun/mirrorr/static/css/main.8628d3a1.chunk.css"
+  },
+  {
+    "revision": "144fc08f40d10add00ce",
+    "url": "/azizantoun/mirrorr/static/js/2.e4a10700.chunk.js"
+  },
+  {
+    "revision": "d99bc364a6097fbaed1e",
+    "url": "/azizantoun/mirrorr/static/js/main.6007468a.chunk.js"
+  },
+  {
+    "revision": "41b0fab43465677afc34",
+    "url": "/azizantoun/mirrorr/static/js/runtime~main.75cbe4fe.js"
+  },
+  {
+    "revision": "62051cf6075d00917d4bd491b894022d",
+    "url": "/azizantoun/mirrorr/static/media/dashboard.62051cf6.png"
+  },
+  {
+    "revision": "5b73b133d3e10becf93c667483d1b447",
+    "url": "/azizantoun/mirrorr/static/media/insights.5b73b133.png"
+  },
+  {
+    "revision": "a6dc2d3733c7222170ee7648b47e3f56",
+    "url": "/azizantoun/mirrorr/static/media/partners.a6dc2d37.png"
+  },
+  {
+    "revision": "f07c7b2e4fad58315a3641da2d96f976",
+    "url": "/azizantoun/mirrorr/static/media/quote.f07c7b2e.png"
+  },
+  {
+    "revision": "cbe02dbdb5e799803853b00f02f7408d",
+    "url": "/azizantoun/mirrorr/static/media/quote2.cbe02dbd.png"
+  }
+]);
